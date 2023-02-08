376d3c28e4f79a92c29b7f638a2a205763ae6606
Clitfor <Clitfor@DESKTOP-MDR2OFU>
2023-02-08 23:35:45 +0100

    Tarea semana 3

---
+++ src/test/java/MLB/html/MlbApplicationTests.java
@@ -0,0 +1,13 @@
+package MLB.html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
